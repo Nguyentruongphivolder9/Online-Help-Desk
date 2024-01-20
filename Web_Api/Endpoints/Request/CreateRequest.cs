b5fd4c51d738e1bb8eabc237bd60d3265d58381e
duyhienn <duyhienn175@gmail.com>
2024-01-20 14:33:34 +0700

    add Request

---
+++ Web_Api/Endpoints/Request/CreateRequest.cs
@@ -0,0 +1,12 @@
+﻿using Application.UseCases.Request.Commands.CreateRequest;
+using Ardalis.ApiEndpoints;
+using SharedKernel;
+
+namespace Web_Api.Endpoints.Request
+{
+    public class CreateRequest: EndpointBaseAsync
+        .WithRequest<CreateRequestCommand>
+        .WithActionResult<Result>
+    {
+    }
+}
