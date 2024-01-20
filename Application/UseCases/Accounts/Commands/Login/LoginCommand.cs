5765c64634fce3ac70012b109a8b03f784494509
phi_ironman <nguyentruongphi15032003@gmail.com>
2024-01-20 23:39:46 +0700

    login

--- Application/UseCases/Accounts/Commands/Login/LoginCommand.cs
+++ Application/UseCases/Accounts/Commands/Login/LoginCommand.cs
@@ -3,10 +3,5 @@ using Application.DTOs;
 
 namespace Application.UseCases.Accounts.Commands.Login
 {
-    //LoginCommand is declared as a sealed (non-inheritable) record.
-    //In C#, records are a concise way to declare immutable types with value-based equality
-
-    //It implements the ICommand<LoginResponse> interface, indicating that this class represents
-    //a command that produces a result of type LoginResponse upon execution.
-    public sealed record LoginCommand(string Email, string Password) : ICommand<LoginResponse>;
+    public sealed record LoginCommand(string AccountId, string Password) : ICommand<LoginResponse>;
 }
