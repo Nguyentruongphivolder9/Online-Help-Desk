148449c3894a42d66a2042020dc113b2e4f5429f
phi_ironman <nguyentruongphi15032003@gmail.com>
2024-01-29 23:36:31 +0700

    update hasdata

--- Infrastructure/Data/Configurations/AccountConfiguration.cs
+++ Infrastructure/Data/Configurations/AccountConfiguration.cs
@@ -26,19 +26,19 @@ namespace Infrastructure.Data.Configurations
 
             builder.HasData(new Account[]
             {
-                new Account { AccountId = "ST729729", RoleId = 1, Password = "@abcOHD123", FullName = "Johnny Depp", 
+                new Account { AccountId = "ST729729", RoleId = 1, Password = "$2a$12$TMO/oJcz01OhMREeWrL9eOTn.K4sGISJhtrIYCmdq1LVserqXvMnS", FullName = "Johnny Depp", 
                     Email = "student@gmail.com", Address = "Bình Chánh", PhoneNumber = "0909009009", Gender = "Male", 
                     Birthday = "30/04/1975", StatusAccount = "Active", Enable = true, CreatedAt = DateTime.UtcNow },
-                new Account { AccountId = "TC729729", RoleId = 2, Password = "@abcOHD123", FullName = "Johnny Dark", 
+                new Account { AccountId = "TC729729", RoleId = 2, Password = "$2a$12$TMO/oJcz01OhMREeWrL9eOTn.K4sGISJhtrIYCmdq1LVserqXvMnS", FullName = "Johnny Dark", 
                     Email = "teacher@gmail.com", Address = "Bình Dương", PhoneNumber = "0909009009", Gender = "Female", 
                     Birthday = "02/09/1945", StatusAccount = "Verifying", Enable = true, CreatedAt = DateTime.UtcNow },
-                new Account { AccountId = "AS729729", RoleId = 3, Password = "@abcOHD123", FullName = "Johnny Đãng", 
+                new Account { AccountId = "AS729729", RoleId = 3, Password = "$2a$12$TMO/oJcz01OhMREeWrL9eOTn.K4sGISJhtrIYCmdq1LVserqXvMnS", FullName = "Johnny Đãng", 
                     Email = "assignees@gmail.com", Address = "Bình Định", PhoneNumber = "0909009009", Gender = "Orther", 
                     Birthday = "07/05/1954", StatusAccount = "Active", Enable = true, CreatedAt = DateTime.UtcNow },
-                new Account { AccountId = "FH729729", RoleId = 4, Password = "@abcOHD123", FullName = "Johnny Bruno", 
+                new Account { AccountId = "FH729729", RoleId = 4, Password = "$2a$12$TMO/oJcz01OhMREeWrL9eOTn.K4sGISJhtrIYCmdq1LVserqXvMnS", FullName = "Johnny Bruno", 
                     Email = "facility@gmail.com", Address = "Alaska", PhoneNumber = "0909009009", Gender = "Orther", 
                     Birthday = "30/04/1945", StatusAccount = "Active", Enable = true, CreatedAt = DateTime.UtcNow },
-                new Account { AccountId = "AD729729", RoleId = 5, Password = "@Phi729729", FullName = "Johnny Đặng", 
+                new Account { AccountId = "AD729729", RoleId = 5, Password = "$2a$12$tGpNXYvvyOQcRNDXepEZe.umYLLDzFYGTN3zDJ3JE0TC7EafEVVF6", FullName = "Johnny Đặng", 
                     Email = "nguyentruongphi15032003@gmail.com", Address = "Alaska", PhoneNumber = "0937888707", Gender = "Orther", 
                     Birthday = "30/04/1945", StatusAccount = "Active", Enable = true, CreatedAt = DateTime.UtcNow },
             });
