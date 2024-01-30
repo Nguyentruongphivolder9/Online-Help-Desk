c31736c8002ef9f8c04181dda9220f6449073171
duyhienn <duyhienn175@gmail.com>
2024-01-30 12:37:54 +0700

    udpate getRequest

--- Domain/Repositories/IRequestRepository.cs
+++ Domain/Repositories/IRequestRepository.cs
@@ -10,7 +10,9 @@ namespace Domain.Repositories
         Task<DataResponse<Request>> GetAllRequestSSFP
             (string? searchTerm, string? sortColumn, string? sortOrder,
             int page, int pageSize, CancellationToken cancellationToken);
-
+        Task<DataResponse<Request>> GetAllClientRequestSSFP
+           (string? searchTerm, string? sortColumn, string? sortOrder,
+           int page, int pageSize, CancellationToken cancellationToken);
         Task<Request?> GetRequestById(Guid id);
     }
 }
---
+++ Web_Api/Endpoints/Requests/GetAllClientRequest.cs
@@ -0,0 +1,42 @@
+﻿using Application.UseCases.Requests.Queries.GetAllClientRequest;
+using Application.UseCases.Requests.Queries.GetAllRequest;
+using Ardalis.ApiEndpoints;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SharedKernel;
+
+namespace Web_Api.Endpoints.Requests
+{
+    public class GetAllClientRequest : EndpointBaseAsync
+        .WithRequest<FieldSSFPClient>
+        .WithActionResult<Result>
+    {
+        private readonly IMediator Sender;
+
+        public GetAllClientRequest(IMediator sender)
+        {
+            Sender = sender;
+        }
+
+        [HttpGet("api/request")]
+        public async override Task<ActionResult<Result>> HandleAsync(
+            [FromQuery] FieldSSFPClient request,
+            CancellationToken cancellationToken = default)
+        {
+            var status = await Sender.Send
+                (new GetAllClienRequestQueries(
+                request.SearchTerm, request.SortColumn,
+                request.SortOrder, request.Page, request.Limit));
+            return Ok(status);
+        }
+    }
+    public class FieldSSFPClient
+    {
+        public string? SearchTerm { get; set; }
+        public string? SortColumn { get; set; }
+        public string? SortOrder { get; set; }
+        public int Page { get; set; }
+        public int Limit { get; set; }
+    }
+
+}
