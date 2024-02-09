075063252b936054ff6706ed1bd69af4a2e74aef
phi_ironman <nguyentruongphi15032003@gmail.com>
2024-02-10 02:16:07 +0700

    check email and phone number

---
+++ Application/UseCases/Accounts/Queries/CheckPhoneNumber/CheckPhoneNumberQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using Application.Common.Messaging;
+using Domain.Repositories;
+using SharedKernel;
+
+namespace Application.UseCases.Accounts.Queries.CheckPhoneNumber
+{
+    public sealed class CheckPhoneNumberQueryHandler : IQueryHandler<CheckPhoneNumberQuery>
+    {
+        private readonly IUnitOfWorkRepository _repo;
+
+        public CheckPhoneNumberQueryHandler(IUnitOfWorkRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<Result> Handle(CheckPhoneNumberQuery request, CancellationToken cancellationToken)
+        {
+            var acc = await _repo.accountRepo.GetByPhoneNumber(request.PhoneNumber);
+            if (acc != null)
+                return Result.Failure(new Error("Error.Client", "data duplication"), "Phone number already exists.");
+
+            return Result.Success("Valid phone number.");
+        }
+    }
+}
