f6f6ed8f72da8ad247293f94676e1513463cfd8c
phi_ironman <nguyentruongphi15032003@gmail.com>
2024-02-26 18:49:08 +0700

    fix field

--- Domain/Entities/Departments/Department.cs
+++ Domain/Entities/Departments/Department.cs
@@ -4,7 +4,7 @@
     {
         public Guid Id { get; set; }
         public string DepartmentName { get; set; }
-        public bool StatusDeppartment { get; set; }
+        public bool StatusDepartment { get; set; }
         public List<Room>? Rooms { get; set; }
     }
 }
