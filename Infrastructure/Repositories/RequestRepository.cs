01be9f85f0768d282803ee5c206d8d2f80fd672a
phi_ironman <nguyentruongphi15032003@gmail.com>
2024-02-26 21:26:42 +0700

    update

--- Infrastructure/Repositories/RequestRepository.cs
+++ Infrastructure/Repositories/RequestRepository.cs
@@ -408,7 +408,7 @@ namespace Infrastructure.Repositories
 
             Expression<Func<Request, object>> keySelector = sortColumn?.ToLower() switch
             {
-                _ => request => request.CreatedAt,
+                _ => request => request.UpdateAt!,
             };
 
             if (sortOrder?.ToLower() == "asc")
